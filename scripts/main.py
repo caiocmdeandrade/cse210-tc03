7c55a7061ab7472de5ceaeac883dc078f04414d4
Caio Andrade <caiocma3@hotmail.com>
2021-09-30 19:08:43 -0700

    creating main.py and andrade.py

---
+++ scripts/main.py
@@ -0,0 +1,12 @@
+'''
+Team: 21f-16
+Purpose: Make a program that sources from multiple files.
+
+'''
+import andrade
+
+def main():
+    print("Welcome to our collaborative program.")
+    andrade.name()
+
+main()
